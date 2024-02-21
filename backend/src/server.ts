46ec72849fc1802fe354567760370920d99ed00f
Michael Uzan <looply3@gmail.com>
2024-02-21 12:26:33 +0200

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+// start the server
+app.listen(process.env.BACK_PORT, () => {
+  console.log(
+    `server running : http://${process.env.BACK_HOST}:${process.env.BACK_PORT}`
+  );
+});
+
+// Please note that to access an env variable, follow the syntax:
+// process.env.NAME_OF_YOUR_VARIABLE_DECLARED_IN_THE_ENV_FILE
